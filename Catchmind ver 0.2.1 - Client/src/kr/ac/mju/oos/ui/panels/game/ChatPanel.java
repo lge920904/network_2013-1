1000bbbb46d7393ed99583034217c249f7183996
leegaeul <lge920904@naver.com>
2013-05-17 16:09:16 +0900

    zzznn

--- Catchmind ver 0.2.1 - Client/src/kr/ac/mju/oos/ui/panels/game/ChatPanel.java
+++ Catchmind ver 0.2.1 - Client/src/kr/ac/mju/oos/ui/panels/game/ChatPanel.java
@@ -4,18 +4,19 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import kr.ac.mju.oos.constants.Constants;
+import kr.ac.mju.oos.uility.ChatTool;
 
 
 public class ChatPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private JTextField field;
+	private ChatTool chatTool;
 	private JTextArea area;
 
 	public ChatPanel() {
@@ -31,37 +32,38 @@ public class ChatPanel extends JPanel {
 
 		setField();
 		setArea();
+		//	this.add(new JLabel("Chat"), BorderLayout.NORTH);		
+		chatTool = new ChatTool();
 
-		this.add(new JLabel("Chat"), BorderLayout.NORTH);		
 	}
 
 	public void setField(){
-		JButton enterBtn;
 		final int TEXTFIELDWIDTH = 40;
 
 		//field.setSize(Constants.PANELS_CHAT_WIDTH,50);
 		field = new JTextField(TEXTFIELDWIDTH);
 		field.addActionListener(new ChatHandler(field));
 		this.add(field, BorderLayout.SOUTH);
-		
+
 	}
 	public void setArea(){
 		area = new JTextArea(Constants.PANELS_CHAT_WIDTH,100);
 		this.add(area, BorderLayout.CENTER);
 	}
-}
-
-class ChatHandler implements ActionListener{
-	private JTextField chatText;
-	public ChatHandler (JTextField chatText){
-		this.chatText = chatText;
+	public void chatMessage(String message){
+		chatTool.sendChat(message);
 	}
-	public void actionPerformed(ActionEvent e) {
-	//	ClientSender cs = new ClientSender(socket, "name")
-		//cs.send(chatText.getText());
-		System.out.println(chatText.getText());
-		chatText.setText("");	
-		chatText.requestFocus();
+	private class ChatHandler implements ActionListener{
+		private JTextField chatText;
+		public ChatHandler (JTextField chatText){
+			this.chatText = chatText;
+		}
+		public void actionPerformed(ActionEvent e) {
+			System.out.println(chatText.getText());
+			chatMessage(chatText.getText());
+			chatText.setText("");	
+			chatText.requestFocus();
+		}
 	}
 }
 
