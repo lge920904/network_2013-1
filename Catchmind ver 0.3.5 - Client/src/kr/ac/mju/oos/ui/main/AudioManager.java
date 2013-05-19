acdf49ac249d759f1bc69dcd1613eb73de391c23
leegaeul <lge920904@naver.com>
2013-05-20 03:18:52 +0900

    audio

--- Catchmind ver 0.3.5 - Client/src/kr/ac/mju/oos/ui/main/AudioManager.java
+++ Catchmind ver 0.3.5 - Client/src/kr/ac/mju/oos/ui/main/AudioManager.java
@@ -1,9 +1,15 @@
 package kr.ac.mju.oos.ui.main;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Sequencer;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import kr.ac.mju.oos.constants.Constants.AUDIO_PLAYLIST;
 import kr.ac.mju.oos.ui.dialogs.LoginDialog;
@@ -13,24 +19,30 @@ public class AudioManager {
 	private Sequencer sequencer;
 	private File file;
 	private AUDIO_PLAYLIST playlist;
+	private AudioInputStream audioInputStream;
+	private Clip clip;
+	private ArrayList<File> file_ArrayList;
 
 	public AudioManager() {
+		file_ArrayList = new ArrayList<File>();
+		for(AUDIO_PLAYLIST playlist : AUDIO_PLAYLIST.values()){
+			file = new File(playlist.getAudio_Url());
+			file_ArrayList.add(file);
+			System.out.println(file_ArrayList.get(0));
+		}
 	}
 
 	public void selectMusic(Object object) {
 		try {
 			if (object instanceof LoginDialog) {
-				file = new File(playlist.login.getAudio_Url());
-				play(file);
-			} else if (object instanceof MainFrame) {
-				file = new File(playlist.game.getAudio_Url());
-				play(file);
-			}
+				play(file_ArrayList.get(0));
+			} 
 			// else if(object instanceof Wait){
-			// if (sequencer!=null)sequencer.stop();
-			// file = new File(playlist.wait.getAudio_Url());
-			// play(file);
+			// play(fileList.get(1));
 			// }
+			else if (object instanceof MainFrame) {
+				play(file_ArrayList.get(2));
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -49,6 +61,17 @@ public class AudioManager {
 			sequencer.open();
 			sequencer.setLoopCount(Sequencer.LOOP_CONTINUOUSLY);
 			sequencer.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void buttonSound(){
+		try {
+			audioInputStream = AudioSystem.getAudioInputStream(file_ArrayList.get(1).getAbsoluteFile());
+			clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
