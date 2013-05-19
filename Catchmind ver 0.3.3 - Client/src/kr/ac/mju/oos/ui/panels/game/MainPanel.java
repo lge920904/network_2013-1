a0de46d4bc6b0d3334156daef70b7c0bec81f25d
kyusung <exteria@naver.com>
2013-05-19 18:48:00 +0900

    toolbox, modify constans

---
+++ Catchmind ver 0.3.3 - Client/src/kr/ac/mju/oos/ui/panels/game/MainPanel.java
@@ -0,0 +1,49 @@
+package kr.ac.mju.oos.ui.panels.game;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+import kr.ac.mju.oos.constants.Constants;
+import kr.ac.mju.oos.controller.ChatController;
+import kr.ac.mju.oos.controller.FrontController;
+
+public class MainPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+
+	private ToolboxPanel toolboxPanel; // ���
+	private CanvasPanel canvasPanel; // ĵ����
+	private BottomPanel bottomPanel; // ä��, �¿��� �÷��̾��Ʈ
+	private FrontController controller;
+
+	public MainPanel() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+
+	public void init() {
+
+		// create Panels
+		controller = new FrontController();
+		toolboxPanel = new ToolboxPanel();
+		canvasPanel = new CanvasPanel();
+		bottomPanel = new BottomPanel();
+
+		// set Layout
+		this.setPreferredSize(new Dimension(Constants.FRAMES_MAIN_WIDTH,
+				Constants.FRAMES_MAIN_HEIGHT));
+		this.setLayout(new BorderLayout());
+
+		this.add(toolboxPanel, "North");
+		this.add(canvasPanel, "Center");
+		this.add(bottomPanel, "South");
+
+		toolboxPanel.init();
+		canvasPanel.init(controller);
+		bottomPanel.init(controller);
+
+		controller.init(canvasPanel.getDrawingTool().getCanvasController(),
+				(ChatController) bottomPanel.getControllers().get(0));
+	}
+}
---
+++ Catchmind ver 0.3.3 - Client/src/kr/ac/mju/oos/ui/panels/wait/RoomListPanel.java
@@ -0,0 +1,17 @@
+package kr.ac.mju.oos.ui.panels.wait;
+
+//대기방 추후
+import javax.swing.JPanel;
+
+public class RoomListPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+
+	public RoomListPanel() {
+		// TODO Auto-generated constructor stub
+		init();
+	}
+
+	public void init() {
+
+	}
+}
